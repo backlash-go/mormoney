b80124168703af19bdf77056219efda2c68f13df
gejiawangluo <gejiawangluo@aadeMacBook-Pro.local>
2021-08-10 19:45:04 +0800

    asd

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">money</router-link>
 
     <router-link to="/labels">labels</router-link>
@@ -15,5 +15,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav {
+  border: 1px solid red;
+}
 </style>
