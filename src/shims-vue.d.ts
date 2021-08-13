d052003b3afdfa183ab4e76e00c15c6a084b0c04
gejiawangluo <gejiawangluo@aadeMacBook-Pro.local>
2021-08-13 17:03:10 +0800

    add nav

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,10 @@
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import Vue from 'vue';
+  export default Vue;
 }
 
 
-
-declare module "*.svg" {
+declare module '*.svg' {
   const content: any;
   export default content;
 }
