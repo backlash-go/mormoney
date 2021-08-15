dacec9fe46083f09769d150e9f09b4dca3583c36
backlash <xixianbing@hexiaoxiang.com>
2021-08-15 15:52:13 +0800

    add css

--- src/App.vue
+++ src/App.vue
@@ -9,41 +9,18 @@
 </template>
 
 <style lang="scss">
-//@import "~@/assets/styles/test.scss";
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-body{
-  line-height: 1.5;
-}
-
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  line-height: 1.5;
+  font-family: $font-hei;
+  color: #333;
+  font-size: 16px;
+
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,54 @@
 <template>
   <Layout>
-    <p>money vue</p>
+    <div class="tags">
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+    </div>
+
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text">
+      </label>
+    </div>
+
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+
+    <div class="numberPad">
+      <div class="output">1000</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+        <button>OK</button>
+        <button>0</button>
+        <button>.</button>
+      </div>
+
+    </div>
   </Layout>
 </template>
 
@@ -12,5 +60,21 @@ export default {
 
 <style lang="scss" scoped>
 
+.tags {
+  font-size: 14px;
+  > .current {
+    display: flex;
+    > li {
+      display: flex;
+      align-items: center;
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      border-radius: $h/2;
+      padding: 0 20px;
+      margin-right: 12px;
+    }
+  }
+}
 
 </style>
