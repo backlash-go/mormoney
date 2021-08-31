960aed40a6bd1a19df778e64959168e8c67e2e7f
gejiawangluo <gejiawangluo@aadeMacBook-Pro.local>
2021-08-31 18:30:55 +0800

    add editlabel function realize

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -11,6 +11,9 @@ type TagListModel = {
   save: () => void,
   create: (name: string) => string,
   fetch: () => Tag[],
+  update: (id: string, name: string) => 'success' | 'notFound' | 'duplicated',
+  remove: (id: string) => boolean
+
 
 }
 
@@ -34,7 +37,35 @@ const tagListModel: TagListModel = {
     this.data.push({id: name, name: name});
     this.save();
     return name;
+  },
+  update(id: string, name: string) {
+    if (name === '') {
+      return 'notFound'
+    }
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.id = tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'notFound';
+    }
+
+  },
+  remove(id: string) {
+    const index = this.data.map((item => item.id)).indexOf(id);
+    this.data.splice(index, 1);
+    this.save();
+    return true;
+
   }
+
 };
 
 export {tagListModel};
